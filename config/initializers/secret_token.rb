03cacbeb3cdafb10b7bc21f7420fccc6bcbb8639
John Rees <john@bitsushi.com>
2013-10-16 12:28:36 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fablabs::Application.config.secret_key_base = '6420047123bb6130141ad3b794db3f3a6762721cd32da4cded5e328a458e37ea71c5f12ad8e943da582144f995ed7b653cda9fec5c2fea48ed6c68cacf090a2c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fablabs::Application.config.session_store :cookie_store, key: '_fablabs_session'
