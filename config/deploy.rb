e06b98b6ee6e90d3eb2b9cb183386180e7ad53a1
John Rees <john@bitsushi.com>
2013-11-20 23:00:21 +0000

    fixed database

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,49 +3,44 @@ require "puma/capistrano"
 
 role :app, "tesla.fablabs.io"
 role :web, "tesla.fablabs.io"
-role :db, "sagan.fablabs.io", primary: true
+# role :db, "sagan.fablabs.io", primary: true
 
 set :application, "fablabs"
-set :user, ENV["deploy_user"]
+set :user, "deployer"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
-
+set :rake, "#{rake} --trace"
 set :scm, "git"
 set :repository, "git@github.com:johnrees/#{application}.git"
 set :branch, "master"
+set :default_environment, {
+  'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
+}
+
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
-namespace :figaro do
-  desc "SCP transfer figaro configuration to the shared folder"
-  task :setup do
-    transfer :up, "config/application.yml", "#{shared_path}/application.yml", :via => :scp
-  end
-
-  desc "Symlink application.yml to the release path"
-  task :finalize do
-    run "ln -sf #{shared_path}/application.yml #{release_path}/config/application.yml"
-  end
-end
-
 namespace :deploy do
 
   task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     run "mkdir -p #{shared_path}/config"
+    transfer :up, "config/application.yml", "#{shared_path}/config/application.yml", :via => :scp
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     # put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
     puts "Now edit the config files in #{shared_path}."
   end
   after "deploy:setup", "deploy:setup_config"
 
-  # task :symlink_config, roles: :app do
-  #   run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  # end
-  # after "deploy:finalize_update", "deploy:symlink_config"
+  task :symlink_config, roles: :app do
+    run "ln -sf #{shared_path}/config/application.yml #{release_path}/config/application.yml"
+    # run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+  # after "deploy:finalize_update", "figaro:finalize"
 
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
