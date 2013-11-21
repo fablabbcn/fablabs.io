4296da473bb1e6bbeacd67b338558c98098a388c
John Rees <john@bitsushi.com>
2013-11-21 16:26:25 +0000

    get to the bottom of the page footer! :hammer:

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
+# load Rails for env vars
+require File.expand_path('../application', __FILE__)
 require "bundler/capistrano"
 
 set :recipes, "config/recipes"
-
 # postgresql
 # monit
-
 %w(base logs figaro blacklist nginx logs unicorn nodejs rbenv security check).each do |r|
   load "#{recipes}/#{r}"
 end
@@ -12,8 +12,9 @@ end
 server "tesla.fablabs.io", :web, :app, :db, primary: true
 # server "sagan.fablabs.io", :db
 
+set :port, ENV['SSH_PORT']
 set :application, "fablabs"
-set :user, "deployer"
+set :user, ENV['DEPLOY_USER']
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
--- config/recipes/figaro.rb
+++ config/recipes/figaro.rb
@@ -1,9 +1,25 @@
 namespace :figaro do
-  desc "Symlink application.yml"
-  task :symlink do
-    run "mkdir -p #{shared_path}/config"
-    transfer :up, "config/application.yml", "#{shared_path}/config/application.yml", :via => :scp
-    run "ln -sf #{shared_path}/config/application.yml #{release_path}/config/application.yml"
+  desc "SCP transfer figaro configuration to the shared folder"
+  task :setup do
+    transfer :up, "config/application.yml", "#{shared_path}/config/application.yml", via: :scp
   end
-  after "deploy:finalize_update", "figaro:symlink"
+
+  desc "Symlink application.yml to the release path"
+  task :symlink do
+    run "ln -sf #{shared_path}/config/application.yml #{latest_release}/config/application.yml"
+  end
+
+  desc "Check if figaro configuration file exists on the server"
+  task :check do
+    begin
+      run "test -f #{shared_path}/config/application.yml"
+    rescue Capistrano::CommandError
+      unless fetch(:force, false)
+        logger.important 'application.yml file does not exist on the server "shared/config/application.yml"'
+        exit
+      end
+    end
+  end
 end
+after "deploy:setup", "figaro:setup"
+after "deploy:create_symlink", "figaro:symlink"
