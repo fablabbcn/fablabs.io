ca285659d21aec4854d8d6edacd9ac6c4954ea26
John Rees <john@bitsushi.com>
2013-11-25 23:57:53 +0000

    cdn host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,11 +55,11 @@ Fablabs::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "http://cdn.fablabs.io"
+  # config.action_controller.asset_host = "//cdn.fablabs.io"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( sessions.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
