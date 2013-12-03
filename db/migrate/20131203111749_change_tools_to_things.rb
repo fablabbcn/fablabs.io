38d242aded7079d0d265fc858f624e31fc0138a0
John Rees <john@bitsushi.com>
2013-12-03 11:57:08 +0000

    machines typo

--- db/migrate/20131203111749_change_tools_to_things.rb
+++ db/migrate/20131203111749_change_tools_to_things.rb
@@ -1,6 +1,6 @@
 class ChangeToolsToThings < ActiveRecord::Migration
   def change
-    rename_table :machines, :things
+    rename_table :tools, :things
     add_column :things, :type, :string
     add_column :things, :inventory_item, :boolean, default: false
     add_index :things, [:id, :type, :inventory_item]
