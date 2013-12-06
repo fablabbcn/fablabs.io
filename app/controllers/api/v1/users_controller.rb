039d6f1d671691ce063873d75af42a612026834d
John Rees <john@bitsushi.com>
2013-12-06 20:05:59 +0000

    doorkeeper

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::UsersController < Api::V1::ApiController
 
- # doorkeeper_for :all
+ doorkeeper_for :all
 
   def show
-    expose User.find(2)#current_user
+    expose current_user
   end
 
 end
