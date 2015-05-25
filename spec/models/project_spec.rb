5344e236d7728f4d31640bc46afab31a9d0ea3c4
hiromipaw <silvia@nopressure.co.uk>
2015-05-25 21:39:00 +0200

    Fixing Tests

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:project) { FactoryGirl.create(:project) }
+
+  it { should have_one(:title) }
+  it { should have_one(:description) }
+  it { should have_one(:github) }
+  it { should have_one(:web) }
+  it { should have_one(:bitbucket) }
+  it { should have_one(:dropbox) }
+  it { should have_many(:contributions) }
+  it { should have_many(:contributors).through(:contributions) }
+  it { should belong_to(:owner) }
+  it { should belong_to(:lab) }
+
+  it "is valid" do
+    expect(lab).to be_valid
+  end
+
+  it "has to_s" do
+    expect(FactoryGirl.build_stubbed(:project, title: "Great New Project").to_s).to eq("Great New Project")
+  end
+
+  
 end
