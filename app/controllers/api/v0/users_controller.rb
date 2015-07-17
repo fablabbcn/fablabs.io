63baeb1526afc2b123d4eda5078c0020d31d0e46
Silvia <silvia@nopressure.co.uk>
2015-07-17 22:56:55 +0200

    CSS fixes

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -5,7 +5,7 @@ class Api::V0::UsersController < Api::V0::ApiController
   end
 
   def search
-    @users = User.where("first_name LIKE ?", "%#{params[:username]}%")  
+    @users = User.where("first_name LIKE ?", "%#{ params[:username].capitalize }%")
     render json: @users, each_serializer: UserSerializer
   end
 
