99e5b4bc6ac8c00be3d9c83d21c4c778d444f155
Silvia <silvia@nopressure.co.uk>
2015-08-03 12:05:11 +0200

    workflow refactoring

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -1,6 +1,8 @@
 class Lab < ActiveRecord::Base
   include RocketPants::Cacheable
   include Authority::Abilities
+  include LabWorkflow
+  
   self.authorizer_name = 'LabAuthorizer'
   resourcify
   has_ancestry
@@ -21,38 +23,6 @@ class Lab < ActiveRecord::Base
   friendly_id :slug_candidates, use: :slugged
   def slug_candidates
     [:name]
-  end
-
-  include Workflow
-  workflow do
-    state :unverified do
-      event :referee_approve, transition_to: :referee_approved
-      event :approve, transitions_to: :approved
-      event :need_more_info, transitions_to: :more_info_needed
-      event :reject, transitions_to: :rejected
-    end
-    state :more_info_needed do
-      event :add_more_info, transitions_to: :more_info_added
-      event :referee_approve, transition_to: :referee_approved
-      event :approve, transitions_to: :approved
-      event :reject, transitions_to: :rejected
-    end
-    state :more_info_added do
-      event :need_more_info, transitions_to: :more_info_needed
-      event :referee_approve, transition_to: :referee_approved
-      event :approve, transitions_to: :approved
-      event :reject, transitions_to: :rejected
-    end
-    state :referee_approved do
-      event :approve, transitions_to: :approved
-      event :need_more_info, transitions_to: :more_info_needed
-      event :reject, transitions_to: :rejected
-    end
-    state :approved do
-      event :remove, transitions_to: :removed
-    end
-    state :rejected
-    state :removed
   end
 
   has_many :academics
