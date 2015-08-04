6096ddaff73951212ac5edcd5f1a03d3b16cb29b
Silvia <silvia@nopressure.co.uk>
2015-08-04 15:34:18 +0200

    fixes again

--- app/mailers/referee_mailer.rb
+++ app/mailers/referee_mailer.rb
@@ -5,12 +5,12 @@ class AdminMailer < ActionMailer::Base
   %w(submitted approved rejected referee_approved more_info_added).each do |action|
     define_method("lab_#{action}") do |lab_id|
       begin
+        @lab = Lab.find(lab_id)
         if @lab.referee_id
           @referee = @lab.referee
           users = (@referee.direct_admins + [@referee.creator]).compact.uniq
           users.each do |user|
-            @user = user
-            mail(to: @user.email_string, subject: "[Fablabs.io] #{@lab} #{action.capitalize}")
+            mail(to: user.email_string, subject: "[Fablabs.io] #{@lab} #{action.capitalize}")
           end
         end
       rescue ActiveRecord::RecordNotFound
