5176de4198b8fc527003c49ab2c4ed5f04f50260
Silvia <silvia@nopressure.co.uk>
2015-08-04 16:48:25 +0200

    Approved and pending labs

--- app/controllers/concerns/labs_operations.rb
+++ app/controllers/concerns/labs_operations.rb
@@ -3,9 +3,9 @@ module LabsOperations
   extend ActiveSupport::Concern
 
   def with_approved_or_pending_state(lab_id)
-    lab = Lab.find(lab_id)
+    lab = Lab.friendly.find(lab_id)
     if ['approved', 'referee_approved', 'more_info_needed', 'more_info_added'].include? lab.workflow_state
-      lab.friendly
+      lab
     else
       raise ActiveRecord::RecordNotFound
     end
