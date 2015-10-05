0b90c0cf12d34b62aec8034c3f782c443ccf4a36
hiromipaw <silvia@nopressure.co.uk>
2015-10-05 16:12:02 +0200

    cleaning up controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,11 +2,10 @@ class SearchController < ApplicationController
 
   before_filter :require_login, except: :index
 
-  @results = Lab.where("slug LIKE ? or name LIKE ?", "%#{params[:q]}%", "%#{params[:q].capitalize}%")
-  @results << Project.where("title LIKE ?", "%#{params[:q]}%")
-
   def index
-    respond_to do |format|
-      format.html { render :locals => { @results, each_serializer: SearchResultSerializer } }
-    end
+    @results = Lab.where("slug LIKE ? or name LIKE ?", "%#{params[:q]}%", "%#{params[:q].capitalize}%")
+    @results << Project.where("title LIKE ?", "%#{params[:q]}%")
+
+    render :locals => { @results, each_serializer: SearchResultSerializer } 
+
   end
