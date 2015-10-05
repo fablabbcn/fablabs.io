d45f51e3efd40dbb18e3fdab49aeb0242ef5c1c9
hiromipaw <silvia@nopressure.co.uk>
2015-10-05 16:07:01 +0200

    local rendering, decoupling api from monolith

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,11 @@ class SearchController < ApplicationController
 
   before_filter :require_login, except: :index
 
+  @results = Lab.where("slug LIKE ? or name LIKE ?", "%#{params[:q]}%", "%#{params[:q].capitalize}%")
+  @results << Project.where("title LIKE ?", "%#{params[:q]}%")
+
   def index
     respond_to do |format|
-      format.html
+      format.html { render :locals => { @results, each_serializer: SearchResultSerializer } }
     end
   end
