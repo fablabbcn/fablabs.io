ef5bcb2381e510cbb0e7ce519417c26f13a52b98
hiromipaw <silvia@nopressure.co.uk>
2015-10-05 15:07:14 +0200

    Another typo in controller

--- app/controllers/api/v0/search_controller.rb
+++ app/controllers/api/v0/search_controller.rb
@@ -4,7 +4,6 @@ class Api::V0::SearchController < Api::V0::ApiController
     @results = Lab.where("slug LIKE ? or name LIKE ?", "%#{params[:q]}%", "%#{params[:q].capitalize}%")
     @results << Project.where("title LIKE ?", "%#{params[:q]}%")
 
-    @results = @resuts.page(params['page']).per(params['per'])
     respond_to do |format|
       format.html { render :template => 'search/all' }
       format.json { render json: @results, each_serializer: SearchResultSerializer }
