1aa56929045b8453814ecd5d0c9c96d8e2ec36bf
hiromipaw <silvia@nopressure.co.uk>
2015-10-27 13:30:48 +0100

    starting to open up  machines

--- app/authorizers/thing_authorizer.rb
+++ app/authorizers/thing_authorizer.rb
@@ -1,11 +1,11 @@
 class ThingAuthorizer < ApplicationAuthorizer
 
   def self.creatable_by?(user)
-    user.has_role? :superadmin
+    user.has_role?(:admin) || user.has_role?(:superadmin)
   end
 
   def self.updatable_by?(user)
-    user.has_role? :superadmin
+    user.has_role?(:admin) || user.has_role?(:superadmin)
   end
 
   def self.readable_by?(user)
