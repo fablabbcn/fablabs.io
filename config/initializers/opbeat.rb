4d2052d2dceecb44fbd9de70738c85f1df6e159e
hiromipaw <silvia@nopressure.co.uk>
2016-06-19 18:05:48 +0200

    Fix opbeat config

--- config/initializers/opbeat.rb
+++
@@ -1,8 +0,0 @@
-require "opbeat"
-
-# set up an Opbeat configuration
-config = Opbeat::Configuration.new do |conf|
-  conf.organization_id = ENV["OPBEAT_ORG_ID"]
-  conf.app_id = ENV["OPBEAT_APP_ID"]
-  conf.secret_token = ENV["OPBEAT_SECRET"]
-end
