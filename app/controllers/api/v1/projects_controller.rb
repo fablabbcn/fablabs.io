c4a96fc89dc8720cd2b621cb85fa911ee5424e07
hiromipaw <silvia@nopressure.co.uk>
2016-08-08 12:00:23 +0200

    Fix projects endpoint

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::ProjectsController < Api::V1::ApiController
   def index
-    @projects = Project.all.joins(:collaborations, :contributions).includes(:lab).references(:lab)
+    @projects = Project.all.includes(:lab, :collaborations, :contributions)
     render json: @projects, each_serializer: ProjectJsonapiSerializer, root: "data"
 
   end
 
   def show
-    @project = Project.joins(:collaborations, :contributions).includes(:lab).references(:lab).find(params[:id])
+    @project = Project.find(params[:id])
     render json: @project, serializer: ProjectJsonapiSerializer, root: "data"
   end
 end
