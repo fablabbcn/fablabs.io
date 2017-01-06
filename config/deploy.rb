0a307dd6a5fb5f3c2f6fb2a5730b1460b442cfbd
Jose Galisteo <ceritium@gmail.com>
2017-01-06 15:23:34 +0100

    remove newrelic references

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :bundle_flags,   "--deployment --verbose --without development test"
 set :rails_env, "production"
 set :recipes, "config/recipes"
 # monit varnish redis
-%w(newrelic varnish memcached monit base postgresql sidekiq redis logs figaro blacklist nginx logs unicorn nodejs rbenv security check mosh).each do |r|
+%w(varnish memcached monit base postgresql sidekiq redis logs figaro blacklist nginx logs unicorn nodejs rbenv security check mosh).each do |r|
   load "#{recipes}/#{r}"
 end
 
