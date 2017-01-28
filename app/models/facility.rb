c3ee9602ba0de9ab49b28f2b540adc10c0f666a0
Jose Galisteo <ceritium@gmail.com>
2017-01-28 11:23:26 +0100

    Fix machine create

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,5 +1,4 @@
 class Facility < ActiveRecord::Base
   belongs_to :lab, touch: true
   belongs_to :thing, touch: true
-  validates_presence_of :thing, :lab
 end
--- spec/models/facility_spec.rb
+++ spec/models/facility_spec.rb
@@ -3,6 +3,4 @@ require 'spec_helper'
 describe Facility do
   it { should belong_to(:thing) }
   it { should belong_to(:lab) }
-  it { should validate_presence_of(:thing) }
-  it { should validate_presence_of(:lab) }
 end
