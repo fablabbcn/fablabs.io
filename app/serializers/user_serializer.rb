286bd92ecef523716fa3f3373e6e5dcb042de772
Jose Galisteo <ceritium@gmail.com>
2017-01-28 17:43:12 +0100

    Avatar url

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :first_name, :last_name, :email, :avatar, :fab10_coupon_code, :fab10_claimed_at, :fab10_cost
+  attributes :id, :username, :first_name, :last_name, :email, :avatar_url, :fab10_coupon_code, :fab10_claimed_at, :fab10_cost
   has_many :employees
 end
