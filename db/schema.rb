c260bd13ecad69d4cee9845e76562cca0c81fff2
Joran <joran.k@gmail.com>
2017-09-07 16:17:09 +1200

    missing delayed_jobs migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223133150) do
+ActiveRecord::Schema.define(version: 20170907041536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,6 +114,22 @@ ActiveRecord::Schema.define(version: 20170223133150) do
 
   add_index "coupons", ["code"], name: "index_coupons_on_code", using: :btree
   add_index "coupons", ["user_id"], name: "index_coupons_on_user_id", using: :btree
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "discussions", force: :cascade do |t|
     t.string   "title"
