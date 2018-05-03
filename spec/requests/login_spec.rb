24be2358a71c77d557cbc995d12dcc7c7aac253d
Fiore Basile <fiore.basile@gmail.com>
2018-05-03 18:54:42 +0200

    login page spec

--- spec/requests/login_spec.rb
+++ spec/requests/login_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "Login process", type: :request do
     it "Shows login page" do
       get signin_path
       expect(response).to have_http_status(200)
+      expect(response).to render_template(:new)
     end
   end
 end
