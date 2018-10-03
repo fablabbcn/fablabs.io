88ae444b0fd5d2a0ab784957ae58c77b08c1bfa2
Viktor Smari <viktorsmari@gmail.com>
2018-10-03 16:24:00 +0200

    Fix rubocop warning, FactoryBot syntax change

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
   factory :project do |f|
     f.association :lab
     f.association :owner
-    f.title           "Fablabs.io"
-    f.description     "This is the official page for fablabs.io platform project."
-    f.faq             "Q: Can I contribute to the fablabs portal project.\n
-                     A: Sure just pick one of the issues on github"
-    f.scope           "We want to improve the way you can build and collaborate on Fablabs projects."
+    f.title       { "Fablabs.io" }
+    f.description { "This is the official page for fablabs.io platform project." }
+    f.faq         { "Q: Can I contribute to the fablabs portal project.\n
+                     A: Sure just pick one of the issues on github" }
+    f.scope       { "We want to improve the way you can build and collaborate on Fablabs projects." }
   end
 end
