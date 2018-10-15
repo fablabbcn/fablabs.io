887671438572b3088c22c1a7c448a258f05d0725
Viktor Smari <viktorsmari@gmail.com>
2018-10-15 12:07:09 +0200

    Remove migration files from 2013

--- db/migrate/20131203111749_change_tools_to_things.rb
+++
@@ -1,9 +0,0 @@
-class ChangeToolsToThings < ActiveRecord::Migration
-  def change
-    rename_table :machines, :things
-    add_column :things, :type, :string
-    add_column :things, :inventory_item, :boolean, default: false
-    add_index :things, [:id, :type, :inventory_item]
-    Thing.update_all("type = 'Machine'")
-  end
-end
--- db/migrate/20131206182044_add_slug_index_to_labs.rb
+++
@@ -1,5 +0,0 @@
-class AddSlugIndexToLabs < ActiveRecord::Migration
-  def change
-    add_index :labs, :slug, unique: true
-  end
-end
