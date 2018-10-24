21b39865929dbac7b0a63c83b9ea5f113679bd6c
Viktor Smari <viktorsmari@gmail.com>
2018-10-24 13:37:38 +0200

    Fix backup script

--- scripts/backup-docker-db.sh
+++ scripts/backup-docker-db.sh
@@ -1,1 +1,1 @@
-docker exec -t fablabsio_db_1 pg_dumpall -c -U postgres > dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+docker exec fablabsio_db_1 pg_dumpall -c -U postgres > dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
