edc74f1c16cbc96189fd220c738990987ed5c2fd
Viktor Smari <viktorsmari@gmail.com>
2019-01-11 14:32:12 +0100

    Production log: debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
   config.force_ssl = false
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
