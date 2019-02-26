2a89ce5c5483c4e997326f528c6cf95f7989cd35
Fiore Basile <fiore.basile@gmail.com>
2019-02-26 12:13:25 +0100

    add email verified field to users api

--- app/serializers/api_user_serializer.rb
+++ app/serializers/api_user_serializer.rb
@@ -1,6 +1,8 @@
-class ApiUserSerializer   < ApiV2Serializer
-    set_type :user
-    attributes :id, :username, :first_name, :last_name, :email, :avatar_url
-    has_many :employees
+# frozen_string_literal: true
+
+class ApiUserSerializer < ApiV2Serializer
+  set_type :user
+  attributes :id, :username, :first_name, :last_name, :email, :avatar_url
+  has_many :employees
+  attribute :email_verified, &:verified?
 end
-  
--- spec/controllers/api/v2/user_controller_spec.rb
+++ spec/controllers/api/v2/user_controller_spec.rb
@@ -1,30 +1,31 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-describe Api::V2::UserController, :type => :request do
+describe Api::V2::UserController, type: :request do
   default_version 2
 
   let!(:user) { FactoryBot.create :user }
   let!(:admin) { FactoryBot.create :user }
 
-  context "When not authenticated"
-  it "Does not allow to access a user profile as an anonymous user" do
-    get  'http://api.fablabs.dev/2/users/' + user.username
+  context 'When not authenticated'
+  it 'Does not allow to access a user profile as an anonymous user' do
+    get 'http://api.fablabs.dev/2/users/' + user.username
     expect(response.status).to eq(401)
     expect(response.content_type).to eq(Mime::JSON)
     # expect(response.parsed_body).to eq({error:"Not authorized"})
   end
 
-
-  it "Does not allow to access a user profile as a normal user" do
+  it 'Does not allow to access a user profile as a normal user' do
     get_as_user 'http://api.fablabs.dev/2/users/' + user.username
     expect(response.status).to eq(403)
     expect(response.content_type).to eq(Mime::JSON)
     # expect(response.parsed_body).to eq({error:"Not authorized"})
   end
 
-  context "When authenticated"
+  context 'When authenticated'
 
-  it "Does allow to get user as a superadmin" do
+  it 'Does allow to get user as a superadmin' do
     get_as_admin 'http://api.fablabs.dev/2/users/' + user.username
     # expect(json['users']).to match_array([user_helper(user)])
     expect(response.status).to eq(200)
@@ -32,19 +33,30 @@ describe Api::V2::UserController, :type => :request do
     # expect(response.parsed_body).to eq({error:"Not authorized"})
   end
 
-  it "Does allow to get the current user profile" do
+  it 'Does allow to get the current user profile' do
     get_as_user 'http://api.fablabs.dev/2/users/me'
     expect(response.status).to eq(200)
     expect(response.content_type).to eq(Mime::JSON)
-    expect(json["data"]["attributes"]["username"]).to eq(user.username)
+    expect(json['data']['attributes']['username']).to eq(user.username)
     # expect(response.parsed_body).to eq({error:"Not authorized"})
   end
 
-  it "gets /0/users.json endpoint without errors" do
+  it 'User profile includes the correct email_verified field' do
+    get_as_user 'http://api.fablabs.dev/2/users/me'
+    expect(response.status).to eq(200)
+    expect(response.content_type).to eq(Mime::JSON)
+    expect(json['data']['attributes']['email_verified']).to be_falsy
+    user.verify!
+    get_as_user 'http://api.fablabs.dev/2/users/me'
+    expect(response.status).to eq(200)
+    expect(response.content_type).to eq(Mime::JSON)
+    expect(json['data']['attributes']['email_verified']).to be_truthy
+  end
+
+  it 'gets /0/users.json endpoint without errors' do
     get_as_user 'http://api.fablabs.dev/0/users.json'
     expect(response.status).to eq(200)
     expect(response.content_type).to eq(Mime::JSON)
-    expect(json.first["first_name"]).to eq("John")
+    expect(json.first['first_name']).to eq('John')
   end
-
 end
