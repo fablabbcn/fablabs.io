883baf0b9703fa91b111582cf76ceda22ce3851b
Fiore Basile <fiore.basile@gmail.com>
2019-03-18 17:42:13 +0100

    init options from figaro

--- app/models/mailchimp_service/client.rb
+++ app/models/mailchimp_service/client.rb
@@ -6,17 +6,21 @@ module MailchimpService
   
         attr_accessor :api_key, :list_id
         
-        def setOptions(options = {})
+        def initialize(options = {})
             @api_key = options[:api_key] || Figaro.env.mailchimp_api_key
             @list_id = options[:list_id] || Figaro.env.mailchimp_list_id
-            @gibbon = Gibbon::Request.new(api_key: api_key)
-            if @list_id.nil?
-                 raise "list_id can't be null"
-            end
+            @gibbon = Gibbon::Request.new(api_key: api_key, debug: true)
+        end
+
+        def setOptions(options = {})
+            self.initialize(options)
         end
 
         def subscribe(user)
-            begin
+            if @list_id.nil?
+                raise "list_id can't be null"
+           end
+           begin
                 @lowercase_email_hash = Digest::MD5.hexdigest(user.email.downcase)
                 res = @gibbon.lists(@list_id).members(@lowercase_email_hash).upsert(body: {
                     email_address: user.email,
@@ -32,7 +36,10 @@ module MailchimpService
 
 
         def members()
-            begin
+            if @list_id.nil?
+                raise "list_id can't be null"
+           end
+           begin
                 return @gibbon.lists(@list_id).members()
             rescue => e
                 puts "Failed to list members: #{e.message} - #{e.raw_body}"
@@ -41,7 +48,10 @@ module MailchimpService
         end
 
         def unsubscribe(user)
-            begin
+            if @list_id.nil?
+                raise "list_id can't be null"
+           end
+           begin
                 @lowercase_email_hash = Digest::MD5.hexdigest(user.email.downcase)
                 res = @gibbon.lists(@list_id).members(@lowercase_email_hash).update(body: {
                     status: "unsubscribed",
