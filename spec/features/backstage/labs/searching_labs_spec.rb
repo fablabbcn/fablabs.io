297ec7f1c70ea1861da6e62cee77e1f369ddfc4e
Fiore Basile <fiore.basile@gmail.com>
2019-10-03 11:48:17 +0200

    disabling spec for projects

---
+++ spec/features/backstage/labs/searching_labs_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+feature "Searching labs" do
+
+  let(:lab) { FactoryBot.create(:lab, name: "iaac") }
+  class Capybara::Session
+    def submit(element)
+      Capybara::RackTest::Form.new(driver, element.native).submit({})
+    end
+  end
+
+  scenario "as a visitor" do
+    visit backstage_labs_path
+    expect(page.title).to match('Sign in')
+  end
+
+  scenario "as a user" do
+    sign_in
+    visit backstage_labs_path
+    expect(current_path).to eq(root_path)
+  end
+
+  scenario "as an admin" do
+    lab.reload
+    sign_in_superadmin
+    visit backstage_labs_path
+    expect(page.title).to match("Labs")
+    fill_in 'q_name_or_city_cont', with: 'iaac' #
+    form = find 'form.lab_search'
+    page.submit form
+    expect(page).to have_link("iaac")
+  end
+
+end
