de657f34d96af31df2e8676a798f2ea0f6c45d56
Viktor Smari <viktorsmari@gmail.com>
2019-11-11 18:57:06 +0100

    Minor gem updates: pg, faraday

--- spec/models/mailchimp_service/client_spec.rb
+++ spec/models/mailchimp_service/client_spec.rb
@@ -29,7 +29,7 @@ describe MailchimpService::Client do
             'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
             'Authorization'=>'Basic YXBpa2V5OjEyMzQtdXMx',
             'Content-Type'=>'application/json',
-            'User-Agent'=>'Faraday v0.15.4'
+            'User-Agent'=>'Faraday v0.17.0'
              }
         stub_request(:put, "#{@api_root}/lists/#{list_id}/members/#{user_hash}").
           with(
