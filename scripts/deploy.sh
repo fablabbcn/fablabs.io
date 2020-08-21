a5eda34bf53c116dddccc5c1c67af0be24976486
Viktor Smari <viktorsmari@gmail.com>
2020-08-21 13:27:43 +0000

    Script: check if migration is needed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,5 @@
 git pull origin master;
 docker-compose build && docker-compose up -d;
 docker-compose exec app rake assets:precompile;
+docker-compose exec app rails db:migrate:status;
 docker-compose restart app;
