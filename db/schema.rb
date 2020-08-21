7ac189bed911c3f8f2460b1b44b585582640cae2
Viktor Smari <viktorsmari@gmail.com>
2020-08-21 22:54:39 +0000

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200821001528) do
+ActiveRecord::Schema.define(version: 2020_08_21_001528) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
   enable_extension "hstore"
+  enable_extension "plpgsql"
 
   create_table "academics", force: :cascade do |t|
     t.integer "user_id"
