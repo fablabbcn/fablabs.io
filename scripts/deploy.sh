367d8a1a8e2d8eb0f606a0486ad6eaefe288a6ba
Viktor Smari <viktorsmari@gmail.com>
2020-08-22 09:05:15 +0000

    Add deploy_history

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,3 +4,5 @@ docker-compose build && docker-compose up -d;
 docker-compose exec app rake assets:precompile;
 docker-compose exec app rails db:migrate:status;
 docker-compose restart app;
+
+echo $(date) $(git rev-parse HEAD) >> deploy_history.txt
