b516cf2ff8542cdb968012bf20e037bb84fa79a8
Viktor Smari <viktorsmari@gmail.com>
2021-01-20 12:07:29 +0100

    Admin dashboard: show last 25 users/labs/jobs

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,31 +2,34 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Recent Users" do
+          ul do
+            User.last(25).map do |item|
+              li link_to(item.full_name, admin_user_path(item))
+            end
+          end
+        end
+      end
+
+      column do
+        panel "Recent Labs" do
+          Lab.last(25).map do |item|
+            li link_to(item.name, admin_lab_path(item))
+          end
+        end
+      end
+
+      column do
+        panel "Recent Jobs" do
+          ul do
+            Job.last(25).map do |item|
+              li link_to(item.title, admin_job_path(item))
+            end
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
   end # content
 end
