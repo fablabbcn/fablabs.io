b0bc90af49ad0c58b9047cac45dea96b1f52f037
Viktor Smari <viktorsmari@gmail.com>
2021-02-11 16:16:04 +0100

    Admin user: allow per_page

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-
+  config.per_page = [50, 100, 300]
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
