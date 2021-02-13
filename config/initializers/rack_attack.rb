4d78d7aedf245ba12575e4a9326505d97af32ee1
Viktor Smari <viktorsmari@gmail.com>
2021-02-13 20:36:43 +0100

    Move banned IPs to a .txt file

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,20 +1,6 @@
-BANNED_IPS = %w[
-  3.6.145.177
-  36.74.42.89
-  36.74.47.177
-  36.75.80.195
-  36.78.20.131
-  36.81.240.106
-  37.120.151.107
-  37.130.224.22
-  89.39.107.146
-  105.112.150.147
-  114.5.217.66
-  114.122.70.56
-  178.162.222.161
-  223.185.42.72
-  223.185.17.90
-]
+return unless File.exist?('BANNED_IPS.txt')
+
+BANNED_IPS = File.readlines('BANNED_IPS.txt', chomp: true)
 
 Rack::Attack.blocklist('block multiple IPS') do |req|
   BANNED_IPS.include?(req.ip)
