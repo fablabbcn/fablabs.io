add763c350fd2f3cf070f79c6d3439539d818468
Viktor Smari <viktorsmari@gmail.com>
2021-02-15 20:48:59 +0100

    Admin: machine per page options

--- app/admin/machines.rb
+++ app/admin/machines.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Machine do
+  config.per_page = [50, 100, 300]
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
