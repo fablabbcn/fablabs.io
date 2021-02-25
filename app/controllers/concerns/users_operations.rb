9f782a09d2bb488e87eef44715e4db9a3816afb0
Viktor Smari <viktorsmari@gmail.com>
2021-02-25 15:57:55 +0100

    Search: Only show verified users

--- app/controllers/concerns/users_operations.rb
+++ app/controllers/concerns/users_operations.rb
@@ -3,5 +3,6 @@ module UsersOperations
 
   def search_users(query)
     User.where("first_name iLIKE ? or last_name iLIKE ? or username iLIKE ?", "%#{query}%", "%#{query}%", query.to_s)
+      .where(workflow_state: :verified)
   end
 end
