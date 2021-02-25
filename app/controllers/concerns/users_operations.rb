ae43335bd5e6ca92e2d5908e463a89e2e5177fff
Viktor Smari <viktorsmari@gmail.com>
2021-02-25 14:50:55 +0100

    User search: case insensitive.
    Closes to #401

--- app/controllers/concerns/users_operations.rb
+++ app/controllers/concerns/users_operations.rb
@@ -1,8 +1,7 @@
 module UsersOperations
-
   extend ActiveSupport::Concern
 
   def search_users(query)
-    User.where("first_name LIKE ? or last_name LIKE ? or username LIKE ?", "%#{query}%", "%#{query}%", "#{query}")
+    User.where("first_name iLIKE ? or last_name iLIKE ? or username iLIKE ?", "%#{query}%", "%#{query}%", query.to_s)
   end
 end
