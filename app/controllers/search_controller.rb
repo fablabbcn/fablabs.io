f29a0bcf3d034b32799da8268fd770077c9a8394
Viktor Smari <viktorsmari@gmail.com>
2021-02-25 15:29:29 +0100

    Search cleanup: paginate 7 users and 7 labs

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,13 +6,20 @@ class SearchController < ApplicationController
   before_action :require_login, except: :index
 
   def index
-    @labs = search_labs(params[:query]).page(params['page']).per(params['per'] || 10)
+    @labs = search_labs(params[:query])
+      .page(params['page'])
+      .per(params['per'] || 7)
+
     #@projects = search_projects(params[:query])
+
     @users = search_users(params[:query])
+      .page(params['page'])
+      .per(params['per'] || 7)
+
     @results = @labs + @users
+
     respond_to do |format|
       format.html
     end
   end
-
 end
