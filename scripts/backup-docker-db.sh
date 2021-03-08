af3e98d6a288d137eb426e8cfa6064bf70a86c05
Viktor Smari <viktorsmari@gmail.com>
2021-03-08 16:04:46 +0100

    Backup do backup/ folder

--- scripts/backup-docker-db.sh
+++ scripts/backup-docker-db.sh
@@ -1,1 +1,1 @@
-docker exec fablabsio_db_1 pg_dumpall -c -U postgres > dump_`date +%Y-%m-%d"_"%H_%M_%S`.sql
+docker exec fablabsio_db_1 pg_dumpall -c -U postgres > backup/dump_`date +%Y-%m-%d"_"%H_%M_%S`.sql
