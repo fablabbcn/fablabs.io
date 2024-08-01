d17f0be94486da2dbeb9147dda2bc8206971b3c9
Julian Gallimore <julian@mactwister.com>
2024-08-01 10:50:56 +0200

    Needs admin access see a lab review page
    Not sure why review page was allowed for none logged in.

--- app/controllers/backstage/my_labs_controller.rb
+++ app/controllers/backstage/my_labs_controller.rb
@@ -1,6 +1,4 @@
 class Backstage::MyLabsController < Backstage::BackstageController
-  skip_before_action :require_admin, only: :index
-
   def index
     @q = current_user.created_labs.ransack(params[:q], auth_object: set_ransack_auth_object)
 
