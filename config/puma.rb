31a26a29333eab1c4e568c5342ef2d909c3573ef
Julian Gallimore <julian@mactwister.com>
2025-07-20 14:46:52 +0200

    WIP new production docker setup

--- config/puma.rb
+++ config/puma.rb
@@ -1,29 +1,37 @@
+# Puma can serve each request in a thread from an internal thread pool.
+# The `threads` method setting takes two numbers: a minimum and maximum.
+# Any libraries that use thread pools should be configured to match
+# the maximum value specified for Puma. Default is set to 5 threads for minimum
+# and maximum; this matches the default thread size of Active Record.
+#
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
 
-workers ENV.fetch('PUMA_WORKERS', `nproc`).to_i
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+#
+port        ENV.fetch("PORT") { 3000 }
 
-max_threads = ENV.fetch('PUMA_MAX_THREADS', 16).to_i
-min_threads = ENV.fetch('PUMA_MIN_THREADS', max_threads).to_i
-threads min_threads, max_threads
+# Specifies the `environment` that Puma will run in.
+#
+environment ENV.fetch("RAILS_ENV") { "development" }
 
-bind 'tcp://0.0.0.0:3000'
-preload_app!
+# Specifies the `pidfile` that Puma will use.
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-# Don't wait for workers to finish their work. We might have long-running HTTP requests.
-# But docker gives us only 10 seconds to gracefully handle our shutdown process.
-# This settings tries to shut down all threads after 2 seconds. Puma then gives each thread
-# an additional 5 seconds to finish the work. This adds up to 7 seconds which is still below
-# docker's maximum of 10 seconds.
-# This setting only works on Puma >= 3.4.0.
-force_shutdown_after 2 if respond_to?(:force_shutdown_after)
+# Specifies the number of `workers` to boot in clustered mode.
+# Workers are forked webserver processes. If using threads and workers together
+# the concurrency of the application would be max `threads` * `workers`.
+# Workers do not work on JRuby or Windows (both of which do not support
+# processes).
+#
+# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
-on_worker_boot do
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-end
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory.
+#
+# preload_app!
 
-# As we are preloading our application and using ActiveRecord
-# it's recommended that we close any connections to the database here to prevent connection leakage
-# This rule also applies to any connections to external services (Redis, databases, memcache, ...)
-# that might be started automatically by the framework.
-before_fork do
-  ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
-end
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
